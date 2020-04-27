17228896c5e8487346c0c17c2c80c3f96e400663
Satyam Kumar Pandey <54197914+satyampundi@users.noreply.github.com>
2020-04-27 17:55:10 +0530

    prob 6

---
+++ sum_square_difference.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	// sum of square of n natural numbers
+	long int  sum1=(100*101*201)/6;
+	long int sum2=pow((101*(100/2)),2);
+     cout<<sum1<<" "<<sum2<<endl;
+    cout<<sum2-sum1;
+			
+	return 0;
+}
+	
