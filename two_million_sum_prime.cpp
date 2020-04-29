2e3cff741172d559211c03055fd5bb050c9da2f3
Satyam Kumar Pandey <54197914+satyampundi@users.noreply.github.com>
2020-04-29 18:25:36 +0530

    answer 10 project euler

---
+++ two_million_sum_prime.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+ #include <iostream>
+using namespace std;
+ 
+long long is_prime(long long n)
+{
+	
+	for (long long i = 3; i <= (pow(n,0.5)); i += 2)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+ 
+int main()
+{
+	unsigned long long sum = 0;
+ 
+	for (int i = 3; i < 2000000; i += 2)
+	{
+		if (is_prime(i))
+		{
+			sum += i;
+		}
+	}
+ 
+	cout << sum + 2;
+ 
+	return 0;
+}
